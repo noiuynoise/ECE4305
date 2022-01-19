613d09229af20dbee91f9b3ccb46271b1a4ed732
noiuynoise <andrew.mularoni@rocketmail.com>
2022-01-19 17:25:26 -0500

    set scale correctly

--- fft.py
+++ fft.py
@@ -5,7 +5,7 @@ import matplotlib
 import time
 
 sample_rate = 54e6 # Hz
-center_freq = 425e6 #107.9e6 # Hz
+center_freq = 850e6 #107.9e6 # Hz
 
 sdr = adi.Pluto("ip:192.168.2.1")
 sdr.sample_rate = int(sample_rate)
@@ -16,11 +16,11 @@ sdr.rx_buffer_size = 1024 # this is the buffer the Pluto uses to buffer samples
 data = np.zeros((1024,1024))
 
 for i in range(1024):
-    data[i] = abs(np.fft.fft(sdr.rx()))
+    data[i] = abs(np.fft.fftshift(np.fft.fft(sdr.rx())))
     #time.sleep(0.01)
 freq = np.fft.fftfreq(1024, d=1/sample_rate)
-print(freq)
-plt.imshow(data)
+#print(freq)
+ax = plt.imshow(data, extent=[-sample_rate/2 + center_freq, sample_rate/2 + center_freq,1024,0], aspect='auto')
 plt.show()
 print(data)
 
