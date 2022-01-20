0080572841beaf151afe5faf1bcacdd72dae7c9d
noiuynoise <andrew.mularoni@rocketmail.com>
2022-01-20 02:30:09 -0500

    made waterfall live, added scans of DSMX transmitter, FM radio, and BLE scan

--- fft.py
+++ fft.py
@@ -5,7 +5,7 @@ import matplotlib
 import time
 
 sample_rate = 54e6 # Hz
-center_freq = 850e6 #107.9e6 # Hz
+center_freq = 2.45e9 #107.9e6 # Hz
 
 sdr = adi.Pluto("ip:192.168.2.1")
 sdr.sample_rate = int(sample_rate)
@@ -14,13 +14,19 @@ sdr.rx_lo = int(center_freq)
 sdr.rx_buffer_size = 1024 # this is the buffer the Pluto uses to buffer samples
 
 data = np.zeros((1024,1024))
-
 for i in range(1024):
     data[i] = abs(np.fft.fftshift(np.fft.fft(sdr.rx())))
-    #time.sleep(0.01)
-freq = np.fft.fftfreq(1024, d=1/sample_rate)
-#print(freq)
-ax = plt.imshow(data, extent=[-sample_rate/2 + center_freq, sample_rate/2 + center_freq,1024,0], aspect='auto')
-plt.show()
-print(data)
+fg = plt.figure()
+ax = fg.gca()
+h = ax.imshow(data, extent=[-sample_rate/2 + center_freq, sample_rate/2 + center_freq,1024,0], aspect='auto')
+
+roll_speed = 20
+
+while True:
+    for i in range(1024 - roll_speed):
+        data[i] = data[i+roll_speed]
+    for i in range(roll_speed):
+        data[1024 - roll_speed + i] = abs(np.fft.fftshift(np.fft.fft(sdr.rx())))
+    h.set_data(data)
+    plt.draw(), plt.pause(0.01)
 
