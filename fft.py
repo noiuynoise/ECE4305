762d84885d2b20e3f972cece98c0c7ac8329cf92
noiuynoise <andrew.mularoni@rocketmail.com>
2022-01-24 20:21:01 -0500

    automatic brightness. WAY easier to see signals

--- fft.py
+++ fft.py
@@ -1,8 +1,6 @@
 import numpy as np
 import adi
 import matplotlib.pyplot as plt
-import matplotlib
-import time
 
 sample_rate = 54e6 # Hz
 center_freq = 160e6 #107.9e6 # Hz
@@ -11,17 +9,24 @@ sdr = adi.Pluto("ip:192.168.2.1")
 sdr.sample_rate = int(sample_rate)
 sdr.rx_rf_bandwidth = int(sample_rate) # filter cutoff, just set it to the same as sample rate
 sdr.rx_lo = int(center_freq)
-sdr.rx_buffer_size = 1024 # this is the buffer the Pluto uses to buffer samples
 
-data = np.zeros((1024,1024))
-for i in range(1024):
+buffer_length = 3000
+
+sdr.rx_buffer_size = buffer_length # this is the buffer the Pluto uses to buffer samples
+
+num_buffers = 500
+
+sdr.gain_control_mode_chan0 = "slow_attack"
+data = np.zeros((num_buffers,buffer_length))
+for i in range(num_buffers):
     data[i] = abs(np.fft.fftshift(np.fft.fft(sdr.rx())))
 fg = plt.figure()
 ax = fg.gca()
-h = ax.imshow(data, extent=[-sample_rate/2 + center_freq, sample_rate/2 + center_freq,1024,0], aspect='auto')
+print_data = data / np.amax(data)
+h = ax.imshow(print_data, extent=[-sample_rate/2 + center_freq, sample_rate/2 + center_freq,1024,0], aspect='auto', vmin=0, vmax=1)
 ax_slider = plt.axes([0.20, 0.01, 0.65, 0.03])
-slider = plt.Slider(ax_slider, 'Slide->', 70, 6000)
-roll_speed = 20
+slider = plt.Slider(ax_slider, 'Slide->', 70, 6000, valinit = center_freq / 1e6)
+roll_speed = 10
 
 def update_center(val):
     print(val)
@@ -31,11 +36,14 @@ def update_center(val):
 
 slider.on_changed(update_center)
 
+brightness = np.amax(data)
+
 while True:
-    for i in range(1024 - roll_speed):
+    for i in range(num_buffers - roll_speed):
         data[i] = data[i+roll_speed]
     for i in range(roll_speed):
-        data[1024 - roll_speed + i] = abs(np.fft.fftshift(np.fft.fft(sdr.rx())))
-    h.set_data(data)
+        data[num_buffers - roll_speed + i] = abs(np.fft.fftshift(np.fft.fft(sdr.rx())))
+    print_data = data / np.amax(data)
+    h.set_data(print_data)
     plt.draw(), plt.pause(0.01)
 
