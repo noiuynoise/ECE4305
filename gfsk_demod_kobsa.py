63e75691b33d334d31d8f2c5417e11e2a3ac490f
manbearkobsa <manbearkobsa@gmail.com>
2022-03-01 13:53:03 -0500

    packet detector done

--- gfsk_demod_kobsa.py
+++ gfsk_demod_kobsa.py
@@ -2,16 +2,18 @@ from cmath import phase
 import numpy as np
 import math
 import matplotlib.pyplot as plt
+import scipy
 from helper_funcs import butter_lowpass_filter, butter_lowpass
 from scipy import signal 
+from scipy import integrate
 
 
-sample_rate = 1e6 # Hz
+sample_rate = 2e6 # Hz
 center_freq = 2426e6 # Hz
 fft_size = 2**10
 modulation_index = 2
 #load in sample data
-data = np.fromfile('recorded_1MHz_3.iq', np.complex64)
+data = np.fromfile('recorded_2MHz.iq', np.complex64)
 timescale = np.arange(0, data.size / sample_rate, 1/sample_rate)
 #frequency shift sampled data for testing
 # data = data*np.exp(2j * np.pi * 250e3 * timescale)
@@ -56,48 +58,64 @@ freq_shift_timescale = np.arange(0, bin_offsets.size/sample_rate, 1/sample_rate)
 #make frequency shifting signal to be mixed with data
 freq_shift = np.exp(-1*2j*np.pi*bin_offsets*freq_shift_timescale)
 #mix frequency compensation with data
-cfc_output = data[0:freq_shift.size] * freq_shift
+cfc_output = data[0:freq_shift.size] #* freq_shift
 #low pass data to remove double frequency term
-cfc_output = butter_lowpass_filter(cfc_output, 400e3, sample_rate)
-'''
-#CFC visualization
-plt.plot(np.abs(np.fft.fftshift(np.fft.fft(cfc_input))))
-cfc_waterfall_bins = np.reshape(cfc_output, (-1, fft_size))
-cfc_data = np.zeros(cfc_waterfall_bins.shape)
-for index, bin in enumerate(cfc_waterfall_bins):
-    cfc_data[index] = np.abs(np.fft.fftshift(np.fft.fft(bin)))
+# cfc_output = butter_lowpass_filter(cfc_output, 400e3, sample_rate)
+area_list = []
+mean_area_list = [0]
+num_packets = 0
+start_of_packet = []
+end_of_packet = []
+packet_present = False
+#Integrator Packet Detecting
+for i in range(len(data)):
+    a = i
+    b = i+20
+    area = sum(abs(data[a:b])**2)
+    area_list.append(area)
+    mean_area = np.mean(area_list)
+    end_flag = 0
+    if mean_area > mean_area_list[-1] and packet_present == False:
+        packet_present = True
+        start_of_packet.append(i)
+    if mean_area < mean_area_list[-1] and packet_present == True:
+        packet_present = False
+        end_flag = 1
 
-pre_cfc_data = np.zeros(cfc_bins.shape)
-for index, bin in enumerate(cfc_bins):
-    pre_cfc_data[index] = np.abs(np.fft.fftshift(np.fft.fft(bin)))
-    
-plt.figure()
-plt.title('data after CFC')
-plt.imshow(cfc_data,extent=[-sample_rate/2, sample_rate/2,cfc_waterfall_bins.shape[1],0], aspect='auto')
-plt.figure()
-plt.title('data before CFC')
-plt.imshow(pre_cfc_data,extent=[-sample_rate/2, sample_rate/2,cfc_waterfall_bins.shape[1],0], aspect='auto')
-plt.show()
+    if end_flag == 1:
+        num_packets += 1
+        end_of_packet.append(i)
 
-print('running waterfall FFT')
-fft_size = 2**4
-cfc_waterfall_bins = np.reshape(cfc_output, (-1, fft_size))
-cfc_data = np.zeros(cfc_waterfall_bins.shape)
-for index, bin in enumerate(cfc_waterfall_bins):
-    cfc_data[index] = np.abs(np.fft.fftshift(np.fft.fft(bin)))
+    mean_area_list.append(mean_area)
+
+print(start_of_packet)
+print(end_of_packet)
+print(num_packets)
+
+#Pruning False IDs
+num_removed = 0
+for i in range(len(start_of_packet)):
+    if end_of_packet[i-num_removed]-start_of_packet[i-num_removed] < 200:
+        del start_of_packet[i-num_removed]
+        del end_of_packet[i-num_removed] 
+        num_packets -= 1
+        num_removed += 1
+print(start_of_packet)
+print(end_of_packet)
+print(num_packets)
+
 plt.figure()
-plt.title('data after CFC')
-cfc_data = np.rot90(cfc_data)
-plt.imshow(cfc_data,extent=[cfc_waterfall_bins.shape[1],0,-sample_rate/2, sample_rate/2], aspect='auto')
-'''
+plt.plot(area_list)
+plt.title('area')
+plt.figure()
+plt.plot(mean_area_list)
+plt.title('area')
 
 
 start_samp = 19260
 end_samp = 19580
-ffc_input = cfc_output[start_samp:end_samp]
-#phase_input = np.arctan(np.imag(ffc_input)/np.real(ffc_input))
-phase_input = np.angle(ffc_input)
-print(phase_input)
+ffc_input = cfc_output #[start_samp:end_samp]
+phase_input = np.angle(ffc_input) 
 plt.figure()
 plt.plot(ffc_input)
 plt.title('ffc_input')
@@ -111,58 +129,66 @@ plt.title('phase_input')
 # ax.scatter3D(np.arange(10), np.real(ffc_input[50:60]), np.imag(ffc_input[50:60]))
 
 
-print('running PLL')
+# print('running PLL')
 
 # #FFC 
 # b_coefs = signal.firls(31, [0, 500e3, 510e3, 1e6],[1, 1, 0, 0], fs=2e6)
 # # high_b_co = signal.firls(19, [0, 2000, 2100, 1e6],[1, 1, 0, 0], fs=2e6)
 
-# # #setup loop filter
-# # # loop_filter_state = signal.lfilter_zi(b_coefs, [1])
+# setup loop filter
+# loop_filter_state = signal.lfilter_zi(b_coefs, [1])
 
-# # #setup PLL output array
+#setup PLL output array
 # pll_output_array = np.zeros(ffc_input.shape, dtype=np.complex64)
 # loop_filter_input_array = np.zeros(ffc_input.shape, dtype=np.complex64)
-# # bias_output_array = np.zeros(ffc_input.shape, dtype=np.complex64)
-# # bias_input_array = np.zeros(ffc_input.shape, dtype=np.complex64)
-# # # New Error Calculations
+# bias_output_array = np.zeros(ffc_input.shape, dtype=np.complex64)
+# bias_input_array = np.zeros(ffc_input.shape, dtype=np.complex64)
+# # New Error Calculations
 
-
-current_symbol = 0
-binary_out = []
-phase_dif_2 = []
-previous_phase = 0
-lower_decision_phase = np.pi/2
-upper_decision_phase = 2*np.pi-lower_decision_phase
-print(len(phase_input))
-for index in range(len(phase_input)):
-    current_phase = phase_input[index]
-    previous_phase = phase_input[index-1]
-    phase_dif = abs(abs(current_phase) - abs(previous_phase))
-    phase_dif_2.append(phase_dif)
-    if phase_dif > lower_decision_phase and phase_dif < upper_decision_phase:
-        symbol_change = True
-    else:
-        symbol_change = False
-
-    if symbol_change == True:
-        current_symbol = 1 if current_symbol == 0 else 0
-        binary_out.append(current_symbol)
-    else:
-        binary_out.append(current_symbol)
-plt.figure()
-plt.plot(np.arange(len(phase_dif_2)),phase_dif_2)
-plt.title('phase_dif')     
-        
-#     #generate VCO signal
-#     # DFS_output = np.exp(1j*2*math.pi*2e6+error)
-#     #mix VCO and input signals
-#     # loop_filter_input = DFS_output * sample
+# for index in range(len(phase_input)):
 #     loop_filter_input_array[index] = phase_input[index]
 #     loop_filter_output = signal.lfilter(b_coefs,[1],loop_filter_input_array[0:index+1])
 #     # loop_filter_output, zf = signal.lfilter(b_coefs,[1], loop_filter_input_array[0:index+1], zi=loop_filter_state, axis=-1)
 #     #store loop filter output
 #     pll_output_array[index] = loop_filter_output[-1]
+
+# phase_input = pll_output_array
+
+# current_symbol = 0
+# binary_out = []
+# phase_dif_2 = []
+# previous_phase = 0
+# lower_decision_phase = np.pi/2
+# upper_decision_phase = 2*np.pi-lower_decision_phase
+# print(len(phase_input))
+# for index in range(len(phase_input)):
+#     current_phase = phase_input[index]
+#     previous_phase = phase_input[index-1]
+#     phase_dif = abs(abs(current_phase) - abs(previous_phase))
+#     phase_dif_2.append(phase_dif)
+#     if phase_dif > lower_decision_phase and phase_dif < upper_decision_phase:
+#         symbol_change = True
+#     else:
+#         symbol_change = False
+
+#     if symbol_change == True:
+#         current_symbol = 1 if current_symbol == 0 else 0
+#         binary_out.append(current_symbol)
+#     else:
+#         binary_out.append(current_symbol)
+# plt.figure()
+# plt.plot(np.arange(len(phase_dif_2)),phase_dif_2)
+# plt.title('phase_dif')     
+        
+#     #generate VCO signal
+#     # DFS_output = np.exp(1j*2*math.pi*2e6+error)
+#     #mix VCO and input signals
+#     # loop_filter_input = DFS_output * sample
+    # loop_filter_input_array[index] = phase_input[index]
+    # loop_filter_output = signal.lfilter(b_coefs,[1],loop_filter_input_array[0:index+1])
+    # # loop_filter_output, zf = signal.lfilter(b_coefs,[1], loop_filter_input_array[0:index+1], zi=loop_filter_state, axis=-1)
+    # #store loop filter output
+    # pll_output_array[index] = loop_filter_output[-1]
 #     bias_input_array[index] = loop_filter_output[-1]
 #     bias_output = signal.lfilter(high_b_co,[1],bias_input_array[0:index+1])
 #     bias_output_array[index] = bias_output[-1]
@@ -177,26 +203,26 @@ plt.title('phase_dif')
 # plt.figure()
 # plt.plot(pll_output_array-bias_output_array)   
 # plt.title('PLL - bias')     
-ones = 0
-zeros = 0
-print(binary_out)
-print(len(binary_out))
-for i in range(int(len(binary_out)/2)):
-    sym_1 = binary_out[i*2]
-    sym_2 = binary_out[i*2+1]
-    if sym_1 == 1:
-        ones += 1
-    if sym_2 == 1:
-        ones += 1
-    if sym_1 == 0:
-        zeros += 1
-    if sym_2 == 0:
-        zeros += 1
-    bit = [sym_1,sym_2]
-    print(bit)
-print(ones)
-print(zeros)
-print('plotting')
+# ones = 0
+# zeros = 0
+# print(binary_out)
+# print(len(binary_out))
+# for i in range(int(len(binary_out)/2)):
+#     sym_1 = binary_out[i*2]
+#     sym_2 = binary_out[i*2+1]
+#     if sym_1 == 1:
+#         ones += 1
+#     if sym_2 == 1:
+#         ones += 1
+#     if sym_1 == 0:
+#         zeros += 1
+#     if sym_2 == 0:
+#         zeros += 1
+#     bit = [sym_1,sym_2]
+#     print(bit)
+# print(ones)
+# print(zeros)
+# print('plotting')
 
 
 # timescale2 = np.linspace(0, ffc_input.size / sample_rate, num=len(binary_out))
