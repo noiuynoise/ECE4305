f57deb367ff9729ffc2196b65cf0a5892d30ae5a
noiuynoise <andrew.mularoni@rocketmail.com>
2022-02-28 22:10:34 -0500

    working 2msps DPLL

--- gfsk_demod.py
+++ gfsk_demod.py
@@ -7,16 +7,11 @@ from scipy import signal
 
 sample_rate = 2e6 # Hz
 center_freq = 2426e6 # Hz
-fft_size = 2**10
+fft_size = 2**14
 modulation_index = 2
 #load in sample data
 data = np.fromfile('recorded.iq', np.complex64)
 timescale = np.arange(0, data.size / sample_rate, 1/sample_rate)
-#frequency shift sampled data for testing
-data = data*np.exp(2j * np.pi * 250e3 * timescale)
-#optional - generate fake data
-#timescale = np.arange(0, 0.02, 1/sample_rate)
-#data = np.exp(2j * np.pi * 250e3 * timescale) + 0.25 * np.exp(2j * np.pi * 50e3 * timescale)
 print('running CFC')
 #raised cosine / low pass filter
 #TODO
@@ -53,11 +48,12 @@ bin_offsets = np.repeat(bin_offsets, fft_size)
 #make a timescale for the data
 freq_shift_timescale = np.arange(0, bin_offsets.size/sample_rate, 1/sample_rate)
 #make frequency shifting signal to be mixed with data
-freq_shift = np.exp(-1*2j*np.pi*bin_offsets*freq_shift_timescale)
+freq_shift = np.exp(-1j*np.pi*bin_offsets*freq_shift_timescale)
 #mix frequency compensation with data
-cfc_output = data[0:freq_shift.size] * freq_shift
+#cfc_output = data[0:freq_shift.size] * freq_shift
+cfc_output = data[0:freq_shift.size]
 #low pass data to remove double frequency term
-cfc_output = butter_lowpass_filter(cfc_output, 600e3, sample_rate)
+#cfc_output = butter_lowpass_filter(cfc_output, 800e3, sample_rate)
 '''
 #CFC visualization
 plt.plot(np.abs(np.fft.fftshift(np.fft.fft(cfc_input))))
@@ -90,39 +86,44 @@ cfc_data = np.rot90(cfc_data)
 plt.imshow(cfc_data,extent=[cfc_waterfall_bins.shape[1],0,-sample_rate/2, sample_rate/2], aspect='auto')
 '''
 
-def runpll(ffc_input, vco_gain):
+def runpll(ffc_input, vco_p_gain, vco_i_gain):
     #initialize DPLL
     vco_curr_phase = 0
-    vco_curr_freq = 250e3 #this is in hz - starting frequency
-    #vco_gain = 10 #do math to find this
+    vco_curr_freq = 0 #this is in hz - starting frequency
+    vco_integrator = 0
 
-    #setup loop filter
-    N = 11  # number of taps in the filter
-    a = 100e3  # width of the transition band
-    fc = 500e3 # cutoff frequency
+    #phases for clusters
+    cluster_phases = [0, np.pi/2, np.pi, 3*np.pi/2]
 
-    # design a halfband symmetric low-pass filter
-    filter = signal.firls(N, [0, fc, fc+a, sample_rate/2], [1, 1, 0, 0], fs=sample_rate)
-    loop_filter_state = signal.lfilter_zi(filter, 1)
-
-    print('running PLL')
     #setup PLL output array
     pll_output_array = np.zeros(ffc_input.shape, dtype=np.complex64)
-    loop_filter_input_array = np.zeros(ffc_input.shape, dtype=np.complex64)
-
+    error_array = np.zeros(ffc_input.shape, dtype=np.complex64)
     for index,sample in enumerate(ffc_input):
         #generate VCO signal
-        vco_curr_phase += vco_curr_freq * 2 * np.pi / sample_rate
-        vco_output = np.exp(1j*vco_curr_phase)
-        #mix VCO and input signals
-        loop_filter_input = vco_output * sample
-        loop_filter_input_array[index] = loop_filter_input
-        loop_filter_output, loop_filter_state = signal.lfilter(filter,1, [loop_filter_input], zi=loop_filter_state, axis=-1)
-        #store loop filter output
-        pll_output_array[index] = loop_filter_output[-1]
+        #vco_curr_phase += vco_curr_freq * 2 * np.pi / sample_rate
+        #vco_curr_phase = np.mod(vco_curr_phase, np.pi)
+        #calculate phase-shifted input
+        input_phase = np.angle(sample) + vco_curr_phase
+        #store PLL output
+        pll_output_array[index] = np.abs(sample) * np.exp(1j*input_phase)
+        #phase comparison
+        min_error = 2*np.pi
+        for phase in cluster_phases:
+            error = input_phase - phase
+            while error > np.pi:
+                error -= 2*np.pi
+            while error < -1 * np.pi:
+                error += 2*np.pi
+            if np.abs(min_error) > np.abs(error):
+                min_error = error
+
+        error_array[index] = min_error
+        vco_integrator += min_error
         #update VCO
-        vco_curr_freq = vco_gain * loop_filter_output[-1]
-    return pll_output_array
+        vco_curr_phase -= vco_p_gain * min_error + vco_i_gain * vco_integrator
+
+
+    return pll_output_array, error_array
 
 '''
 for i in range(4, 8):
@@ -132,18 +133,54 @@ for i in range(4, 8):
     plt.title(f'PLL Output - gain={10**i}')
     plt.plot(timescale[6800:8000], np.angle(output[6800:8000]))
 '''
+packet_start = 8400
+packet_end = 8800
 
-pll_output_array = runpll(cfc_output, 250)
+
+pll_output_array, error_array = runpll(cfc_output[packet_start:packet_end], 0.4, 0)
+
+
+plot_start = 0
+plot_end = pll_output_array.size
+'''
+min_error_sum = np.sum(np.power(error_array[plot_start:plot_end],2))
+best_params = (1,1)
+
+for i in np.arange(0,2,0.1):
+    for j in np.arange(0,2,0.1):
+        print(f'running PLL with parameters {(i,j)}')
+        pll_output_array, error_array = runpll(cfc_output, i, j)
+        error_sum = np.sum(np.power(error_array[plot_start:plot_end],2))
+        print(f'error sum: {error_sum}')
+        if error_sum < min_error_sum:
+            min_error_sum = error_sum
+            best_params = (i, j)
+
+print(f'best parameters: {best_params} with error sum of {min_error_sum}')
+'''
 
 print('plotting')
 fig = plt.figure()
 ax = plt.axes(projection='3d')
 #ax.title('Loop Filter Output')
-ax.scatter3D(timescale[22000:24000], np.real(pll_output_array[22000:24000]), np.imag(pll_output_array[22000:24000]))
+ax.scatter3D(timescale[plot_start:plot_end], np.real(pll_output_array[plot_start:plot_end]), np.imag(pll_output_array[plot_start:plot_end]))
 plt.figure()
-plt.title('PLL Output')
-plt.plot(timescale[50:],np.imag(pll_output_array[50:])/np.amax(pll_output_array[50:]))
+plt.title('PLL Error')
+plt.plot(timescale[plot_start:plot_end],error_array[plot_start:plot_end])
+plt.figure()
+plt.title('PLL Input - Phase')
+plt.scatter(timescale[plot_start:plot_end],np.angle(cfc_output[packet_start:packet_end]))
+zero_line = np.full(timescale[plot_start:plot_end].shape, 0)
+pi_2_line = np.full(timescale[plot_start:plot_end].shape, np.pi/2)
+pi_line = np.full(timescale[plot_start:plot_end].shape, np.pi)
+neg_pi_2_line = np.full(timescale[plot_start:plot_end].shape, -np.pi/2)
+neg_pi_line = np.full(timescale[plot_start:plot_end].shape, -np.pi)
+plt.plot(timescale[plot_start:plot_end],zero_line)
+plt.plot(timescale[plot_start:plot_end],pi_2_line)
+plt.plot(timescale[plot_start:plot_end],pi_line)
+plt.plot(timescale[plot_start:plot_end],neg_pi_2_line)
+plt.plot(timescale[plot_start:plot_end],neg_pi_line)
 plt.figure()
 plt.title('PLL Output - Phase')
-plt.plot(timescale[50:],np.angle(pll_output_array[50:]))
+plt.scatter(timescale[plot_start:plot_end],np.angle(pll_output_array[plot_start:plot_end]))
 plt.show()
